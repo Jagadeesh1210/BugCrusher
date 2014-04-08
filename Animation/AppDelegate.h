f0c85d977f34b80ca307f5411b9952a2d4cc287a
Jagadeesh <jagadeesh.ja@spoors.in>
2014-04-08 10:02:42 +0530

    Bug Crusher
    Game

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by ios on 11/09/13.
+//  Copyright (c) 2013 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
